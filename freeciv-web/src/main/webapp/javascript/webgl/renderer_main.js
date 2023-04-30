7e70e1e3282e5cc9f7ad440195b45aebe168cdb8
Andreas Røsdal <andreas.rosdal@gmail.com>
2023-04-30 18:43:53 +0200

    Adjust terrain quality to 8.

--- freeciv-web/src/main/webapp/javascript/webgl/renderer_main.js
+++ freeciv-web/src/main/webapp/javascript/webgl/renderer_main.js
@@ -22,7 +22,7 @@ var QUALITY_HIGH = 3;   // best quality, add features which require high-end gra
 
 var graphics_quality = QUALITY_HIGH;
 
-var terrain_quality = 7; // 8 is too slow.
+var terrain_quality = 8; // 8 is slow, 7 has problems with rivers.
 
 var anaglyph_3d_enabled = false;
 var stats = null;
