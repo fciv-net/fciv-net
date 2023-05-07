f078faf816908cd412e2f03ea2a03ab8fb9f1bc9
Andreas Røsdal <andreas.rosdal@gmail.com>
2023-05-07 17:46:53 +0200

    Cleanup startup scripts.

--- scripts/status-freeciv-web.sh
+++ scripts/status-freeciv-web.sh
@@ -114,13 +114,9 @@ checkWebURL "freeciv-web on Tomcat" "http://localhost:8080/freeciv-web" --head
 checkWebURL "Tomcat DB connection" "http://localhost/game/list" --head
 
 checkWebURL "Pubstatus" "http://localhost:4002/pubstatus"
-checkWebURL "Mailstatus" "http://localhost:4003/mailstatus"
 
 checkWebURL "freeciv-proxy directly" "http://localhost:7001/status"
 checkWebURL "freeciv-proxy through nginx" "http://localhost/civsocket/7001/status"
-
-checkWebSocket "WebSocket directly (freeciv-proxy)" "http://localhost:7002/civsocket/7002"
-checkWebSocket "WebSocket through nginx" "http://localhost/civsocket/7002"
 
 checkPID "freeciv-web (spawned by publite2)" "freeciv-web"
 
