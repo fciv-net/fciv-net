374d5590a344bb6ec42bf3a6305599b0cce01a34
andreasrosdal <andreas.rosdal@gmail.com>
2024-01-24 20:15:16 +0100

    Tomcat 10

--- scripts/install/dependency-services-default-stop.sh
+++ scripts/install/dependency-services-default-stop.sh
@@ -14,7 +14,7 @@ fi
 # 2. Tomcat
 if [ "${TOMCATMANAGER}" != "Y" ]; then
   if service --status-all | grep -Fq 'tomcat10'; then
-    sudo service tomcat10 stop || echo "unable to stop tomcat9 service"
+    sudo service tomcat10 stop || echo "unable to stop tomcat10 service"
   else
     sudo -u tomcat /var/lib/tomcat10/bin/catalina.sh stop
   fi
