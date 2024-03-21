1be0ce40f6a5c848e9d591ccadacc6fe06061e5c
Andreas Røsdal <41650711+andreasrosdal@users.noreply.github.com>
2024-03-21 21:41:21 +0100

    Delete freeciv-web/sync-docker.sh

--- freeciv-web/sync-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Run this script inside the docker container (Exec) in the freeciv-web directory, and it copy files from freeciv-web-shared (files in C:\ in Windows) to freeciv-web (inside Docker container). For local dev environment."
-echo "Then run build.sh to rebuild.";
-
-rsync -avz --update ../freeciv-web-shared/ ../freeciv-web
-
